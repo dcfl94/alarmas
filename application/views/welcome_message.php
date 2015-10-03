6282d78c53ef07910d66637331558516120c2d84
Diana Farfàn <dcfl.94@hotmail.com>
2015-10-03 16:39:18 -0500

    Creación del repositorio
    Se crea el repositorio para ser descargado

---
+++ application/views/welcome_message.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Curso de codigo facilito</title>
+
+	<style >
+	body{
+		font-family: :arial , "Helvetica";
+	}
+
+	</style>
+
+	
+</head>
+<body>
+
+<div id="container">
+	<h1><?= $string ?> vía codeigniter</h1>
+
+	<div id="body">
+		
+</div>
+
+</body>
+</html>
